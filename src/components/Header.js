cc99cd386617649dc72f7ec7cacd19d7845934ce
Carlos <murillocarlos99@hotmail.com>
2021-08-29 14:57:13 -0500

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,7 @@
 import React from "react";
 import "./Header.css";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-  
+import { Link } from "react-router-dom";
+
 const Header = () =>{
     return(
         <nav>
